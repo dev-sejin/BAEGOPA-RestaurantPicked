0e10781619fa452f2e421d90a230f0a279457274
thinkySide <eunlyuing@gmail.com>
2023-05-14 10:54:01 +0900

    [Rename] Location to SearchLocationAPI

---
+++ RestaurantPicked/Models/SearchLocationAPI.swift
@@ -0,0 +1,13 @@
+//
+//  SearchLocationAPI.swift
+//  RestaurantPicked
+//
+//  Created by Sejin on 2023/05/12.
+//
+
+import Foundation
+
+/// 네이버 지역 검색 API 관리 클래스입니다.
+final class SearchLocationAPI {
+    
+}
