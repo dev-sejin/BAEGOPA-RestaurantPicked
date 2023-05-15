0516305cb7f30412502d4a27b87a182faa511d01
Andy An <nationpower81@gmail.com>
2023-05-16 03:11:39 +0900

    <feat>(*): Add SheetView.swift & SheetViewController.swift Files
    - SheetView : Custom SheetView for WKWebView
    - SheetViewController : Controller for SheetView

---
+++ RestaurantPicked/Controllers/SheetViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SheetViewController.swift
+//  RestaurantPicked
+//
+//  Created by Doyoung An on 2023/05/16.
+//
+
+import UIKit
+import WebKit
+
+class SheetViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // custom view 생성 (Views 폴더 -> SheetView: UIView 파일)
+    private let sheetView = SheetView()
+    // view 교체
+    override func loadView() {
+        view = sheetView
+    }
+    
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    // MARK: - Helpers Functions
+    
+    
+    
+    
+    
+    
+    
+}
