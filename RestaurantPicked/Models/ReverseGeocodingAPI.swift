6e35d702d4bcf15bb3c7d15a7f6440971153275b
gkals4417 <gkals4417@gmail.com>
2023-05-16 09:08:36 +0900

    feat: Add ReverseGeocoding API Methods

--- RestaurantPicked/Models/ReverseGeocodingAPI.swift
+++ RestaurantPicked/Models/ReverseGeocodingAPI.swift
@@ -14,11 +14,32 @@ class ReverseGeocodingAPI {
     private init() {}
     
     private let basicURL = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc"
-//    idHeader = "X-NCP-APIGW-API-KEY-ID"
-//    pwHeader = "X-NCP-APIGW-API-KEY"
     
-    func fetchData(coord: String) {
+    typealias ReverseGeocodingNetwork = (GeoWelcome) -> Void
+    
+    func fetchData(coord: String, completion: @escaping ReverseGeocodingNetwork) {
         let url = "\(basicURL)?coords=\(coord)&output=json"
+        
+        getDataFromAPI(url: url) { result in
+            completion(result)
+        }
+    }
+    
+    private func getDataFromAPI(url: String, completion: @escaping ReverseGeocodingNetwork) {
+        let headers: HTTPHeaders = ["X-NCP-APIGW-API-KEY-ID" : Bundle.main.GEOCODING_ID, "X-NCP-APIGW-API-KEY" : Bundle.main.GEOCODING_PW]
+        let request = AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers)
+        
+        request
+            .validate(statusCode: 200..<500)
+            .responseDecodable(of: GeoWelcome.self) { response in
+                switch response.result {
+                case .success(let data):
+                    print(data)
+                    completion(data)
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
     
 }
