4b42a4f98fb0e2f0e6610b16264c0e7886cbe573
thinkySide <eunlyuing@gmail.com>
2023-05-20 12:48:37 +0900

    🐛 지도에 여러번 핀이 꽂히는 현상 수정 #19

--- RestaurantPicked/Controllers/RestaurantDetailViewController.swift
+++ RestaurantPicked/Controllers/RestaurantDetailViewController.swift
@@ -68,86 +68,92 @@ final class RestaurantDetailViewController: UIViewController {
     private func configureLocationData() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest  // 위치 정확도 품질 설정
-        locationManager.startUpdatingLocation()
-        // 전역변수에 사용자의 현재 위치 할당
-        if let location = locationManager.location {
+        
+        /// 직렬 큐 생성
+        let serialQueue = DispatchQueue(label: "serial")
+        
+        /// 동기 처리
+        serialQueue.async { [weak self] in
+            guard let self = self else { return }
+            
+            /// 1. 위치 업데이트 시작
+            locationManager.startUpdatingLocation()
+            
+            /// 2. 전역변수에 사용자의 현재 위치 할당
+            guard let location = locationManager.location else { return }
             myLocation = location
+            
+            /// 3. 검색 및 지도 업데이트
+            updateMapView()
         }
     }
     
     // 맵뷰 설정
     private func configureMapView() {
+        print(#function)
         restaurantDetailView.mapView.delegate = self
         restaurantDetailView.mapView.showsUserLocation = true  // 위치 사용 시 사용자의 현재 위치를 표시
-//        restaurantDetailView.mapView.setUserTrackingMode(.followWithHeading, animated: true)  // 현재 화면을 사용자 위치로 이동 및 확대. (애니메이션 효과로 부드러운 화면 이동)
+        //        restaurantDetailView.mapView.setUserTrackingMode(.followWithHeading, animated: true)  // 현재 화면을 사용자 위치로 이동 및 확대. (애니메이션 효과로 부드러운 화면 이동)
     }
     
+    private func updateMapView() {
+        let lon = String(myLocation.coordinate.longitude)  // 경도 정보
+        let lat = String(myLocation.coordinate.latitude)  // 위도 정보
+        let coord = "\(lon),\(lat)"
+        print(coord)
+        
+        // ReverseGeocodingAPI (좌표 to 주소)
+        ReverseGeocodingAPI.shared.getDataFromAPI(coord: coord) { [unowned self] data in
+            let city = data.results[0].region.area1.name
+            let district = data.results[0].region.area2.name
+            let street = data.results[0].region.area3.name
+            let string = "\(city) \(district) \(street) 맛집"
+            self.webViewURL += "\(street)" // webViewURL 뒤에 주소(동) 추가 (검색 정확도 또는 이름이 같은 식당이 있을 경우를 대비해서)
+            print(string)
+            
+            // SearchLocationAPI (주소+맛집 식당검색)
+            SearchLocationAPI.shared.requestLocation(searchQuery: string) { [unowned self] data in
+                
+                // 네이버 지역 검색 결과를 이용해 랜덤 장소 요청
+                let selectedRestaurant = SearchLocationAPI.shared.getRandomLocation()
+                // 네이버 지역 검색 결과를 이용해 구한 랜덤 장소의
+                // TM128 좌표계를 변환 후 WGS84(위도, 경도) 좌표 요청
+                guard let selectedRestaurantCoord = SearchLocationAPI.shared.getRandomLocationCoordinate() else { return }
+                
+                // 데이터 할당 과정
+                // 식당 레이블 설정
+                if let title = selectedRestaurant?.title {
+                    self.restaurantDetailView.restaurantNameLabel.text = title
+                    print(title)
+                    self.webViewURL.append(title)  // webViewURL 완성
+                }
+                
+                // annotation 생성
+                let restaurnt = SelectRestaurantAnnotaion(title: selectedRestaurant?.title, locationName: selectedRestaurant?.address, discipline: selectedRestaurant?.category, coordinate: selectedRestaurantCoord)
+                restaurantDetailView.mapView.addAnnotation(restaurnt)
+                
+                // Visible Area 설정
+                // 좌표 기준에서 부터 거리(m)를 반영하여 맵의 크기를 가진 객체 생성
+                let coordinateRegion = MKCoordinateRegion(center: selectedRestaurantCoord, latitudinalMeters: 1500, longitudinalMeters: 1500)
+                restaurantDetailView.mapView.setRegion(coordinateRegion, animated: false)
+                
+                // 카메라 제약 설정
+                let center = selectedRestaurantCoord
+                let region = MKCoordinateRegion(center: center, latitudinalMeters: 5000, longitudinalMeters: 6000)
+                restaurantDetailView.mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+                let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+                restaurantDetailView.mapView.setCameraZoomRange(zoomRange, animated: false)
+            }
+        }
+    }
 }
+
 
 
 // MARK: - Extension
 
 // extension CLLocationManagerDelegate Methods
 extension RestaurantDetailViewController: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // 마지막으로 받은 위치정보를(위도와 경도 정보를 담고 있다) 상수에 할당 및 옵셔널 바인딩
-        if let location = locations.last {
-            
-            // 계속해서 위치정보를 가져오는 것을 멈추기
-            //            locationManager.stopUpdatingLocation()
-            
-            let lon = String(location.coordinate.longitude)  // 경도 정보
-            let lat = String(location.coordinate.latitude)  // 위도 정보
-            let coord = "\(lon),\(lat)"
-            print(coord)
-            
-            // ReverseGeocodingAPI (좌표 to 주소)
-            ReverseGeocodingAPI.shared.getDataFromAPI(coord: coord) { [unowned self] data in
-                let city = data.results[0].region.area1.name
-                let district = data.results[0].region.area2.name
-                let street = data.results[0].region.area3.name
-                let string = "\(city) \(district) \(street) 맛집"
-                self.webViewURL.append(street)  // webViewURL 뒤에 주소(동) 추가 (검색 정확도 또는 이름이 같은 식당이 있을 경우를 대비해서)
-                print(string)
-                
-                // SearchLocationAPI (주소+맛집 식당검색)
-                SearchLocationAPI.shared.requestLocation(searchQuery: string) { [unowned self] data in
-                    
-                    // 네이버 지역 검색 결과를 이용해 랜덤 장소 요청
-                    let selectedRestaurant = SearchLocationAPI.shared.getRandomLocation()
-                    // 네이버 지역 검색 결과를 이용해 구한 랜덤 장소의
-                    // TM128 좌표계를 변환 후 WGS84(위도, 경도) 좌표 요청
-                    guard let selectedRestaurantCoord = SearchLocationAPI.shared.getRandomLocationCoordinate() else { return }
-                    
-                    // 데이터 할당 과정
-                    // 식당 레이블 설정
-                    if let title = selectedRestaurant?.title {
-                        self.restaurantDetailView.restaurantNameLabel.text = title
-                        print(title)
-                        self.webViewURL.append(title)  // webViewURL 완성
-                    }
-                    
-                    // annotation 생성
-                    let restaurnt = SelectRestaurantAnnotaion(title: selectedRestaurant?.title, locationName: selectedRestaurant?.address, discipline: selectedRestaurant?.category, coordinate: selectedRestaurantCoord)
-                    restaurantDetailView.mapView.addAnnotation(restaurnt)
-                    
-                    // Visible Area 설정
-                    // 좌표 기준에서 부터 거리(m)를 반영하여 맵의 크기를 가진 객체 생성
-                    let coordinateRegion = MKCoordinateRegion(center: selectedRestaurantCoord, latitudinalMeters: 1500, longitudinalMeters: 1500)
-                    restaurantDetailView.mapView.setRegion(coordinateRegion, animated: false)
-                    
-                    // 카메라 제약 설정
-                    let center = selectedRestaurantCoord
-                    let region = MKCoordinateRegion(center: center, latitudinalMeters: 5000, longitudinalMeters: 6000)
-                    restaurantDetailView.mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
-                    let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
-                    restaurantDetailView.mapView.setCameraZoomRange(zoomRange, animated: false)
-                }
-            }
-        }
-        
-    }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(String(describing: error))
@@ -183,9 +189,9 @@ extension RestaurantDetailViewController: MKMapViewDelegate {
             view.calloutOffset = CGPoint(x: -5, y: 5)
             
             // rightCalloutAccessoryView 버튼 이미지 변경 설정
-//            let mapsButton = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 35, height: 35)))
-//            mapsButton.setBackgroundImage(UIImage(systemName: "figure.walk.motion"), for: .normal)
-//            view.rightCalloutAccessoryView = mapsButton
+            //            let mapsButton = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 35, height: 35)))
+            //            mapsButton.setBackgroundImage(UIImage(systemName: "figure.walk.motion"), for: .normal)
+            //            view.rightCalloutAccessoryView = mapsButton
             
             view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
@@ -199,8 +205,8 @@ extension RestaurantDetailViewController: MKMapViewDelegate {
         guard view.annotation is SelectRestaurantAnnotaion else { return }
         
         // 경로 팝업
-//        let lanuchOtions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
-//        selectRestaurantAnnotaion.mapItem?.openInMaps(launchOptions: lanuchOtions)
+        //        let lanuchOtions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
+        //        selectRestaurantAnnotaion.mapItem?.openInMaps(launchOptions: lanuchOtions)
         
         // SheetView 팝업
         showSheetView()
